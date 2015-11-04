9af9028b658207e78e376f4d138f9227e7b92354
Artur <aptyp@mobility.local>
2015-11-04 15:55:20 -0600

    changes

--- backend/routes/show.js
+++ backend/routes/show.js
@@ -3,11 +3,4 @@
  * GET images
  */
 
-var express = require('express');
-var router = express.Router();
-
-router.GET('/show', function(req, res) {
-	res.send(200);
-});
-
-module.exports = router;
+ 
