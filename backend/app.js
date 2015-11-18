a199dd3323ae8338ab5744b64ba587b351de0c3e
root <root@ozeran.node.js.localdomain>
2015-11-18 14:56:08 -0600

    display first version

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,54 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-var routes = require('./routes');
-
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-app.get('/showme', routes.showme);
-
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+//include http, fs and url module
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
+    imageDir = '/opt/node/gall/backend/storage/';
+ 
+//create http server listening on port 3000 
+http.createServer(function (req, res) {
+    //use the url to parse the requested url and get the image name
+    var query = url.parse(req.url,true).query;
+        pic = query.image;
+ 
+    if (typeof pic === 'undefined') {
+        getImages(imageDir, function (err, files) {
+            var imageLists = '<ul>';
+            for (var i=0; i<files.length; i++) {
+                imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>';
+            }
+            imageLists += '</ul>';
+            res.writeHead(200, {'Content-type':'text/html'});
+            res.end(imageLists);
+        });
+    } else {
+        //read the image using fs and send the image content back in the response
+        fs.readFile(imageDir + pic, function (err, content) {
+            if (err) {
+                res.writeHead(400, {'Content-type':'text/html'})
+                console.log(err);
+                res.end("No such image");    
+            } else {
+                //specify the content type in the response will be an image
+                res.writeHead(200,{'Content-type':'image/jpg'});
+                res.end(content);
+            }
+        });
+    }
+ 
+}).listen(3000);
+console.log("Server running at http://localhost:3000/");
+ 
+//get the list of jpg files in the image dir
+function getImages(imageDir, callback) {
+    var fileType = '.jpg',
+        files = [], i;
+    fs.readdir(imageDir, function (err, list) {
+        for(i=0; i<list.length; i++) {
+            if(path.extname(list[i]) === fileType) {
+                files.push(list[i]); //store the file name into the array files
+            }
+        }
+        callback(err, files);
+    });
+}
