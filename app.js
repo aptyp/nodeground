b1399666338c1d37f625002fe10b04a05feb7e6b
root <root@ozeran.node.js.localdomain>
2015-12-08 19:29:30 -0600

    rolled back to fix multi issue

---
+++ app.js
@@ -0,0 +1,69 @@
+var express	= require('express');
+var path	= require('path');
+var fs		= require('fs');
+var bodyParser	= require('body-parser');
+var multer	= require('multer');
+
+var port 	= 80;
+var app 	= new express();
+
+var imageDir	= path.join(__dirname, 'storage');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+
+function getImages(imageDir,callback) {
+ var files = [], i;
+ fs.readdir(imageDir, function (err, list){
+  for(i=0; i < list.length; i++){
+   files.push(list[i]); 
+   console.log("forloop: " + list[i]); 
+  }
+  callback(null,files);
+ })
+}
+
+
+app.get('/', function(req, res) {
+ res.render('index', { title: 'Awesome Gallery Viewer', pictures: getImages(imageDir, function (err, files){
+  console.log("outside: "+files);
+  return files;
+  })
+ })
+})
+
+app.get('/image/:id/image.jpg', function(req,res) {
+ res.statusCode = 200;
+ res.setHeader('Content-type', 'image/jpeg');
+ res.sendFile(imageDir + '/' + req.params.id);
+})
+
+app.get('/upload', function(req, res){
+  res.render('uploader', { title: 'Awesome Gallery Uploader'});
+});
+
+app.post('/upload', multer({ dest: './storage/'}).single('upl'), function(req,res){
+ console.log(req.body); //form fields
+ /* example output:
+ { title: 'abc' }
+ */
+ console.log(req.file); //form files
+ /* example output:
+ { fieldname: 'upl',
+   originalname: 'grumpy.png',
+   encoding: '7bit',
+   mimetype: 'image/png',
+   destination: './uploads/',
+   filename: '436ec561793aa4dc475a88e84776b1b9',
+   path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+   size: 277056 }
+  */
+ res.status(401).redirect('http://162.210.92.11');
+ //res.status(204).end();
+});
+
+
+
+
+app.listen( port, function(){ console.log('listening on port '+port); } );
